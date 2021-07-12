22c8957d6cf1c421813bb6c3dd1fa38ca760d439
Geraldhi Mahayadnya <geraldiaditya1111@gmail.com>
2021-07-12 15:22:29 +0700

    minifying css

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,16 +1,16 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
+import React from "react";
 
 function CardItem(props) {
   return (
     <>
-      <li className='cards__item'>
-        <div className='cards__item__link'>
-          <div className='cards__item__pic-wrap'>
-            <h1 className='value-display'>{props.value}</h1>
+      <li className="cards__item">
+        <div className="cards__item__link">
+          <div className="cards__item__pic-wrap">
+            <h1 className="value-display">{props.value}</h1>
           </div>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+          <div className="cards__item__info">
+            <h1 className="cards__item__text">{props.text}</h1>
           </div>
         </div>
       </li>
